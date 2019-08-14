a688ae8397b12602ebba26c667480f2f32de23c2
Eric Montzka <emontzka@sbcglobal.net>
2019-08-14 16:36:14 -0500

    clean up

--- src/components/RandomPokemon.js
+++ src/components/RandomPokemon.js
@@ -1,35 +1,54 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import Pokemon from './Pokemon'
 import { handleGeneratePokemon, deleteRandomPokemon } from '../actions/pokemon'
+import Loader from './Loader'
 
 
-function RandomPokemon(props) {
-    console.log('random props ',props)
-    const buttonText = props.randomPoke  ? 'Change Pokemon' : 'Generate Pokemon'
-    const poke = props.randomPoke ? props.randomPoke[Object.keys(props.randomPoke)[0]] : null
-    console.log('poke is ', poke)
-    function handleRandomPokemon() {
-        if (props.randomPoke !== null) {
-            async function change() {
-                await props.dispatch(deleteRandomPokemon('randomPoke'))
-                await props.dispatch(handleGeneratePokemon())
-            }
-            change()
-        } else {
-            props.dispatch(handleGeneratePokemon())
+class RandomPokemon extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isFetching: false
         }
     }
-    return (
-        <div>
-            <button style={{marginBottom: 20}} onClick={() => handleRandomPokemon()}>{buttonText}</button>
-            <div style={{minHeight: 230}}>
-                {poke &&  (
-                    <Pokemon key={poke.id} data={poke} />      
-                )}
+
+    handleRandomPokemon() {
+        this.setState({isFetching: true}, async () => {
+            try {
+                if (this.props.randomPoke !== null) {
+                    await this.props.dispatch(deleteRandomPokemon('randomPoke'))
+                    this.props.dispatch(handleGeneratePokemon())
+                } else {
+                    await this.props.dispatch(handleGeneratePokemon())
+                }
+                this.setState({isFetching: false})
+            } catch (error) {
+                console.log(error)
+            }
+        })
+    }
+    render() {  
+            let buttonText
+            if (this.state.isFetching) {
+                buttonText = 'Fetching...'
+            } else {
+                buttonText = this.props.randomPoke  ? 'Change Pokemon' : 'Generate Pokemon'
+            }
+            const poke = this.props.randomPoke ? this.props.randomPoke[Object.keys(this.props.randomPoke)[0]] : null
+        return (
+            <div>
+                <button style={{marginBottom: 20}} onClick={() => this.handleRandomPokemon()}>{buttonText}</button>
+                {this.state.isFetching && <Loader />}
+                <div style={{minHeight: 230}}>
+                    
+                    {poke &&  (
+                        <Pokemon key={poke.id} data={poke} />      
+                    )}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 function mapStateToProps({randomPoke}) {
